8e383c14d360394e4fad3c70bab2d111c558118c
x8826054 <602385278@qq.com>
2018-12-20 15:25:52 +0800

    提交

---
+++ mapper/src/main/java/com/igniubi/mapper/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * mybatis代码生成应用目录介绍:
+ * dto: 请求/响应实体
+ * model: 与数据库映射的实体
+ * enums: 枚举类
+ * service: 基础增删改业务接口
+ * business: 基于基础service上处理业务逻辑接口
+ * controller: 控制器
+ * <p>
+ *
+ * @author  徐擂
+ * @date    2018-12-20
+ * @version  1.0.0
+ */
+package com.igniubi.mapper;
